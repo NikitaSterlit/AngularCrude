b9aa2ba422c3877edee10401acfaf30ab135e759
Nikita <klerik1995@mail.ru>
2019-05-10 18:20:25 +0300

    Object.assign({}, film);

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // как обращаемся к компоненту <app-root></app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'untitled';
+  myForm: FormGroup;
+  constructor(){
+    this.myForm = new FormGroup({
+
+      "userName": new FormControl("Tom", Validators.required),
+      "userEmail": new FormControl("", [
+        Validators.required,
+        Validators.pattern("[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}")
+      ]),
+      "userPhone": new FormControl()
+    });
+  }
+
+  submit(){
+    console.log(this.myForm);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplatedrivenformComponent } from './templatedrivenform/templatedrivenform.component';
+import { ModeldrivenformComponent } from './modeldrivenform/modeldrivenform.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpformComponent } from './httpform/httpform.component';
+import {HttpClientModule} from '@angular/common/http';
+import {Httpservice} from './httpform/httpservice';
+import { FilmformComponent } from './filmform/filmform.component';
+import {Filmservice} from './filmform/filmservice';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplatedrivenformComponent,
+    ModeldrivenformComponent,
+    HttpformComponent,
+    FilmformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [Httpservice, Filmservice],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
